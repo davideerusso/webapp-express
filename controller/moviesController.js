abc4408d9a565c9ecef68fe726cdfcfc64c61bc5
davideerusso <rd.96.rd@gmail.com>
2025-06-16 16:50:47 +0200

    fix

--- controller/moviesController.js
+++ controller/moviesController.js
@@ -1,7 +1,7 @@
 const connection = require("../data/movieConnection");
 
 const index = (req, res) => {
-  const sql = "SELECT * FROM posts";
+  const sql = "SELECT * FROM movies";
 
   connection.query(sql, (err, results) => {
     console.log(results);
@@ -16,17 +16,18 @@ const index = (req, res) => {
 };
 
 const show = (req, res) => {
-  const postId = parseInt(req.params.id);
+  const movieId = parseInt(req.params.id);
 
-  const sqlPost = `SELECT * FROM posts WHERE id=?`;
+  const sqlMovie = `SELECT * FROM movies WHERE id=?`;
 
-  connection.query(sqlPost, [postId], (err, results) => {
+  connection.query(sqlMovie, [movieId], (err, results) => {
     if (err) return res.status(500).json({ error: "error executing query" });
     if (results.length === 0)
-      return res.status(404).json({ error: "post not found" });
-    res.json({ status: 200, data: post });
+      return res.status(404).json({ error: "movie not found" });
+    const movie = results[0];
+
+    res.json({ status: 200, data: movie });
   });
-  const post = result[0];
 };
 
 module.exports = { index, show };
